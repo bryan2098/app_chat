5221b04a9679a063f86417817820e7b9cfed0480
nguyentri1998 <nguyentri.7264@gmail.com>
2020-11-19 21:23:28 +0700

    auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './screens/auth_screen.dart';
 import './screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter App Chat', theme: ThemeData(), home: ChatScreen());
+    return MaterialApp(
+      title: 'Flutter App Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        backgroundColor: Colors.pink,
+        accentColor: Colors.deepPurple,
+        accentColorBrightness: Brightness.dark,
+      ),
+      home: AuthScreen(),
+    );
   }
 }
